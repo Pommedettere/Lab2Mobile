f6d198163cc8559d7ae9ac88372e7f44caa1e05c
Pommedettere <klim20112001@mail.ru>
2026-02-06 12:03:55 +0400

    Лаб 2

---
+++ app/src/main/java/com/example/mob2lab/ArtworkDataSource.kt
@@ -0,0 +1,21 @@
+package com.example.mob2lab
+
+object ArtworkDataSource {
+    val artworks = listOf(
+        Artwork(
+            title = "Sailing Under the Bridge",
+            author = "Kat Kuan",
+            year = "2017"
+        ),
+        Artwork(
+            title = "Mountain Sunrise",
+            author = "Alex Chen",
+            year = "2020"
+        ),
+        Artwork(
+            title = "Urban Dreams",
+            author = "Maria Rodriguez",
+            year = "2019"
+        )
+    )
+}
